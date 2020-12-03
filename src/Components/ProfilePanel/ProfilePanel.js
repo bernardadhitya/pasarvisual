522461bb73db44b3edc45931c8671da08dace8b3
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-12-03 21:07:41 +0700

    [Bernard] Create profile page

---
+++ src/Components/ProfilePanel/ProfilePanel.js
@@ -0,0 +1,165 @@
+import React from 'react';
+import { useFonts } from '@use-expo/font';
+import { View, Text, StyleSheet } from 'react-native';
+import { Layout } from '@ui-kitten/components';
+import { Fonts } from '../../Constants/Fonts';
+import { AppLoading } from 'expo';
+import { ScrollView } from 'react-native';
+import { DarkColors } from '../../Constants/Colors';
+import { TouchableOpacity } from 'react-native';
+import ShowcasePanel from '../ShowcasePanel/ShowcasePanel';
+import { useNavigation } from '@react-navigation/native';
+
+const ProfilePanel = (props) => {
+  const navigation = useNavigation();
+  const { handleClick, isSelf } = props;
+  let [fontsLoaded] = useFonts(Fonts);
+
+  const renderOthersOption = () => {
+    return (
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <TouchableOpacity 
+            style={{
+              backgroundColor: DarkColors.secondary,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 15,
+              borderRadius: 12
+            }}
+          >
+            <Text style={{
+              color: DarkColors.primary,
+              fontFamily: 'Bold',
+              fontSize: 16,
+              padding: 8
+            }}>Follow</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{width: 10}}></View>
+        <View style={styles.column}>
+          <TouchableOpacity 
+            style={{
+              backgroundColor: DarkColors.secondary,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 15,
+              borderRadius: 12
+            }}
+          >
+            <Text style={{
+              color: DarkColors.primary,
+              fontFamily: 'Bold',
+              fontSize: 16,
+              padding: 8
+            }}>Message</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{width: 10}}></View>
+        <View style={styles.column}>
+          <TouchableOpacity 
+            onPress={() => navigation.navigate('ViewServicesScreen')}
+            style={{
+              backgroundColor: DarkColors.secondary,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 15,
+              borderRadius: 12
+            }}
+          >
+            <Text style={{
+              color: DarkColors.primary,
+              fontFamily: 'Bold',
+              fontSize: 16,
+              padding: 8
+            }}>Services</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+
+  const renderSelfOptions = () => {
+    return (
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <TouchableOpacity 
+          onPress={() => navigation.navigate('EditServicesScreen')}
+            style={{
+              backgroundColor: DarkColors.secondary,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 15,
+              borderRadius: 12
+            }}
+          >
+            <Text style={{
+              color: DarkColors.primary,
+              fontFamily: 'Bold',
+              fontSize: 16,
+              padding: 8
+            }}>Edit Services</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+
+  const renderOptions = () => {
+    return isSelf ? renderSelfOptions() : renderOthersOption()
+  }
+
+  return fontsLoaded ? (
+    <ScrollView>
+      <View style={styles.row, {
+        marginTop: 15,
+        alignItems: 'center'
+      }}>
+        <View style={{
+          borderRadius: 50,
+          height: 100,
+          width: 100,
+          backgroundColor: DarkColors.red
+        }}></View>
+      </View>
+      <View style={styles.row, {
+        marginTop: 15,
+        alignItems: 'center'
+      }}>
+        <Text style={{
+          color: DarkColors["text-primary"],
+          fontFamily: 'Bold',
+          fontSize: 21
+        }}>
+          Bernard Adhitya
+        </Text>
+      </View>
+      { renderOptions() }
+      <View style={{ height: 10 }}></View>
+      <ShowcasePanel handleClick={handleClick}/>
+      <View style={{height: 100}}></View>
+    </ScrollView>
+  ) : <AppLoading/>;
+}
+
+const styles = StyleSheet.create({
+  column: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+  }
+});
+
+export default ProfilePanel
---
+++ src/Containers/ProfilePage/ProfilePage.js
@@ -0,0 +1,113 @@
+import React, { useRef } from 'react';
+import { Text, SafeAreaView, View, StyleSheet } from 'react-native';
+import { Fonts } from '../../Constants/Fonts';
+import { AppLoading } from 'expo';
+import { useFonts } from '@use-expo/font';
+import { ScrollView } from 'react-native';
+import ProfilePanel from '../../Components/ProfilePanel/ProfilePanel';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+import { useMemoOne } from 'use-memo-one';
+import CreativePostDetail from '../../Components/PostPanel/CreativePostDetail';
+import { DarkColors } from '../../Constants/Colors';
+import ButtonBack from '../../Assets/buttons/ButtonBack';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const ProfilePage = () => {
+  const navigation = useNavigation();
+  let [fontsLoaded] = useFonts(Fonts);
+
+  let sheetRef = useRef(null);
+  let fall = useMemoOne(() => new Animated.Value(1), []);
+
+  const renderContent = () => {
+    return (
+      <View
+        style={{
+          backgroundColor: DarkColors["sub-primary"],
+          padding: 16,
+          height: 700
+        }}
+      >
+        <CreativePostDetail role='creative'/>
+      </View>
+    )
+  };
+
+  const renderShadow = () => {
+    const animatedShadowOpacity = Animated.interpolate(fall, {
+      inputRange: [0, 1],
+      outputRange: [0.5, 0],
+    })
+
+    return (
+      <Animated.View
+        pointerEvents="none"
+        style={[
+          styles.shadowContainer,
+          {
+            opacity: animatedShadowOpacity,
+          },
+        ]}
+      />
+    )
+  }
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return ( 
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: DarkColors.background
+        }}
+      >
+        <BottomSheet
+          ref={sheetRef}
+          initialSnap={2}
+          callbackNode={fall}
+          snapPoints={[700, 600, -100]}
+          renderContent={renderContent}
+          borderRadius={16}
+        />
+        <ScrollView style={{paddingHorizontal: 20, paddingTop: 50}}>
+          <ProfilePanel isSelf handleClick={() => sheetRef.current.snapTo(1)}/>
+        </ScrollView>
+        {renderShadow()}
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  column: {
+    flexDirection: 'column',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  layout: {
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  center: {
+    justifyContent: 'center',
+  },
+  shadowContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#000',
+  }
+});
+
+export default ProfilePage;
