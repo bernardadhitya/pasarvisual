0d62e82f521b73d6d7c4e79f9ca89f0c9abcacbe
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-12-03 20:54:22 +0700

    [Bernard] Update Home Page

---
+++ src/Components/HomePanel/HomeCard.js
@@ -0,0 +1,170 @@
+import React from 'react';
+import { useFonts } from '@use-expo/font';
+import { View, Text, StyleSheet } from 'react-native';
+import { Layout } from '@ui-kitten/components';
+import { Fonts } from '../../Constants/Fonts';
+import { AppLoading } from 'expo';
+import { DarkColors } from '../../Constants/Colors';
+import { Touchable } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import IconHeart from '../../Assets/icons/IconHeart';
+import { useNavigation } from '@react-navigation/native';
+
+const HomeCard = (props) => {
+  const { role, image, handleClick, title, description } = props;
+  const navigation = useNavigation();
+  let [fontsLoaded] = useFonts(Fonts);
+
+  return fontsLoaded ? (
+    <View
+      style={{
+        marginTop: 14,
+        backgroundColor: DarkColors["sub-primary"],
+        padding: 20,
+        borderRadius: 24
+      }}
+    >
+      <TouchableOpacity
+        onPress={() => navigation.navigate('OtherProfileScreen')}
+        style={{marginBottom: 15}}
+      >
+        <View style={styles.row}>
+          <View styles={styles.column}>
+            <View style={{
+              backgroundColor: DarkColors.red,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 20,
+              marginRight: 10,
+              height: 40,
+              width: 40
+            }}>
+              <Text>
+                Test
+              </Text>
+            </View>
+          </View>
+          <View style={styles.column}>
+            <Text
+              style={{
+                color: DarkColors["text-primary"],
+                fontFamily: 'Bold',
+                fontSize: 16
+              }}
+            >
+              Name
+            </Text>
+            <Text
+              style={{
+                color: DarkColors["text-secondary"],
+                fontFamily: 'Regular',
+                fontSize: 14
+              }}
+            >
+              20 April at 4:20 PM
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      { image ?
+        <TouchableOpacity
+          onPress={() => handleClick()}
+          style={{
+            marginBottom: 15
+          }}
+        >
+          <View style={{
+            height: 200,
+            backgroundColor: DarkColors["sub-secondary"]
+          }}>
+            
+          </View>
+        </TouchableOpacity>
+        : null
+      }
+      <View style={styles.row}>
+        <View style={styles.column}>
+          {role === 'creative' ? <Text
+            style={{
+              color: DarkColors["text-primary"],
+              fontFamily: 'Semibold',
+              fontSize: 18
+            }}
+          >
+            { title }
+          </Text> : null}
+        </View>
+        <View style={styles.column, {alignSelf: 'flex-end'}}>
+          <View style={styles.row}>
+            { role === 'creative' ?
+              <>
+                <IconHeart/>
+                <View style={{width: 5}}></View>
+                <Text
+                  style={{
+                    color: DarkColors["text-secondary"],
+                    fontFamily: 'Regular',
+                    fontSize: 18
+                  }}
+                >
+                  40 likes
+                </Text>
+              </> : null
+            }
+          </View>
+        </View>
+      </View>
+      <Text
+        style={{
+          color: DarkColors["text-secondary"],
+          fontFamily: 'Regular',
+          fontSize: 16
+        }}
+      >
+        { description }
+      </Text>
+      { role === 'business' ?
+        <TouchableOpacity 
+          onPress={() => handleClick()}
+          style={{
+            backgroundColor: DarkColors.primary,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 15,
+            borderRadius: 12
+          }}
+        >
+          <Text style={{
+            color: DarkColors["text-primary"],
+            fontFamily: 'Bold',
+            fontSize: 16,
+            padding: 8
+          }}>More Details</Text>
+        </TouchableOpacity>
+        : null
+      }
+    </View>
+  ) : <AppLoading/>;
+}
+
+const styles = StyleSheet.create({
+  column: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+  }
+});
+
+export default HomeCard
---
+++ src/Containers/HomePage/HomePage.js
@@ -0,0 +1,151 @@
+import React, { useRef } from 'react';
+import { Text, SafeAreaView, View, StyleSheet } from 'react-native';
+import { Fonts } from '../../Constants/Fonts';
+import { AppLoading } from 'expo';
+import { useFonts } from '@use-expo/font';
+import { ScrollView } from 'react-native';
+import { DarkColors } from '../../Constants/Colors';
+import HomeCard from '../../Components/HomePanel/HomeCard';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+import { useMemoOne } from 'use-memo-one';
+import CreativePostDetail from '../../Components/PostPanel/CreativePostDetail';
+
+
+const HomePage = () => {
+  let [fontsLoaded] = useFonts(Fonts);
+
+  let sheetRef = useRef(null);
+  let fall = useMemoOne(() => new Animated.Value(1), []);
+
+  const renderContent = () => {
+    return (
+      <View
+        style={{
+          backgroundColor: DarkColors["sub-primary"],
+          padding: 16,
+          height: 700
+        }}
+      >
+        <CreativePostDetail role='creative'/>
+      </View>
+    )
+  };
+
+  const renderShadow = () => {
+    const animatedShadowOpacity = Animated.interpolate(fall, {
+      inputRange: [0, 1],
+      outputRange: [0.5, 0],
+    })
+
+    return (
+      <Animated.View
+        pointerEvents="none"
+        style={[
+          styles.shadowContainer,
+          {
+            opacity: animatedShadowOpacity,
+          },
+        ]}
+      />
+    )
+  }
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return ( 
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: DarkColors.background
+        }}
+      >
+        <BottomSheet
+          ref={sheetRef}
+          initialSnap={2}
+          callbackNode={fall}
+          snapPoints={[700, 600, -100]}
+          renderContent={renderContent}
+          borderRadius={16}
+        />
+        <ScrollView style={{paddingHorizontal: 20, paddingTop: 50}}>
+          <Text
+            style={{
+              color: DarkColors["text-primary"],
+              fontFamily: 'Bold',
+              fontSize: 42
+            }}
+          >
+            Feeds
+          </Text>
+          <HomeCard
+            role='creative'
+            image='Sample image'
+            handleClick={() => sheetRef.current.snapTo(1)}
+            title='Weekly progress'
+            description='Weekly progress on dieting'
+          />
+          <HomeCard
+            role='business'
+            handleClick={() => sheetRef.current.snapTo(1)}
+            title='We are looking for talents'
+            description='We’re interested in your ideas and would be glad to build something bigger out of it. Share your ideas about features/design and we’ll bring them on to our full case of this product design.'
+          />
+          <HomeCard
+            role='creative'
+            image='Sample image'
+            handleClick={() => sheetRef.current.snapTo(1)}
+            title='Weekly progress'
+            description='Weekly progress on dieting'
+          />
+          <HomeCard
+            role='business'
+            handleClick={() => sheetRef.current.snapTo(1)}
+            title='We are looking for talents'
+            description='We’re interested in your ideas and would be glad to build something bigger out of it. Share your ideas about features/design and we’ll bring them on to our full case of this product design.'
+          />
+          <HomeCard
+            role='business'
+            handleClick={() => sheetRef.current.snapTo(1)}
+            title='We are looking for talents'
+            description='We’re interested in your ideas and would be glad to build something bigger out of it. Share your ideas about features/design and we’ll bring them on to our full case of this product design.'
+          />
+          <View style={{height: 100}}></View>
+        </ScrollView>
+        {renderShadow()}
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  column: {
+    flexDirection: 'column',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  layout: {
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  center: {
+    justifyContent: 'center',
+  },
+  shadowContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#000',
+  }
+});
+
+export default HomePage;
