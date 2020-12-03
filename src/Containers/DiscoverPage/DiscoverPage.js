ab0e1546cdc4a3fb11b53c2330d1f8fabe6d6ec0
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-12-03 21:08:03 +0700

    [Bernard] Create discover page

---
+++ src/Containers/DiscoverPage/DiscoverPage.js
@@ -0,0 +1,119 @@
+import React, { useRef } from 'react';
+import { Text, SafeAreaView, View, StyleSheet } from 'react-native';
+import { Fonts } from '../../Constants/Fonts';
+import { AppLoading } from 'expo';
+import { useFonts } from '@use-expo/font';
+import { ScrollView } from 'react-native';
+import { DarkColors } from '../../Constants/Colors';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Animated from 'react-native-reanimated';
+import { useMemoOne } from 'use-memo-one';
+import CreativePostDetail from '../../Components/PostPanel/CreativePostDetail';
+import { Input } from '@ui-kitten/components';
+import FilterTopicsPanel from '../../Components/DiscoverPanel/FilterTopicsPanel';
+import ShowcasePanel from '../../Components/ShowcasePanel/ShowcasePanel';
+
+const DiscoverPage = () => {
+  let [fontsLoaded] = useFonts(Fonts);
+
+  let sheetRef = useRef(null);
+  let fall = useMemoOne(() => new Animated.Value(1), []);
+
+  const renderContent = () => {
+    return (
+      <View
+        style={{
+          backgroundColor: DarkColors["sub-primary"],
+          padding: 16,
+          height: 700
+        }}
+      >
+        <CreativePostDetail role='creative'/>
+      </View>
+    )
+  };
+
+  const renderShadow = () => {
+    const animatedShadowOpacity = Animated.interpolate(fall, {
+      inputRange: [0, 1],
+      outputRange: [0.5, 0],
+    })
+
+    return (
+      <Animated.View
+        pointerEvents="none"
+        style={[
+          styles.shadowContainer,
+          {
+            opacity: animatedShadowOpacity,
+          },
+        ]}
+      />
+    )
+  }
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return ( 
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: DarkColors.background
+        }}
+      >
+        <BottomSheet
+          ref={sheetRef}
+          initialSnap={2}
+          callbackNode={fall}
+          snapPoints={[700, 600, -100]}
+          renderContent={renderContent}
+          borderRadius={16}
+        />
+        <ScrollView style={{paddingHorizontal: 20, paddingTop: 50}}>
+          <Input style={{
+            backgroundColor: DarkColors["sub-secondary"],
+            borderColor: DarkColors["sub-primary"]
+          }}
+            placeholder="Search"
+          />
+          <FilterTopicsPanel/>
+          <ShowcasePanel handleClick={() => sheetRef.current.snapTo(1)}/>
+          <View style={{height: 100}}></View>
+        </ScrollView>
+        {renderShadow()}
+      </SafeAreaView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  column: {
+    flexDirection: 'column',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  layout: {
+    justifyContent: 'center',
+    marginVertical: 10
+  },
+  center: {
+    justifyContent: 'center',
+  },
+  shadowContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: '#000',
+  }
+});
+
+export default DiscoverPage;
